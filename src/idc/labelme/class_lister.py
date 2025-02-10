4f036c84a5dfaae3fbf5df8b1bcf6fe1d57b4d37
Peter Reutemann <fracpete@gmail.com>
2025-02-11 11:42:42 +1300

    removed image segmentation placeholders

--- src/idc/labelme/class_lister.py
+++ src/idc/labelme/class_lister.py
@@ -5,12 +5,10 @@ def list_classes() -> Dict[str, List[str]]:
     return {
         "seppl.io.Reader": [
             "idc.labelme.reader.imgcls",
-            "idc.labelme.reader.imgseg",
             "idc.labelme.reader.objdet",
         ],
         "seppl.io.Writer": [
             "idc.labelme.writer.imgcls",
-            "idc.labelme.writer.imgseg",
             "idc.labelme.writer.objdet",
         ],
     }
