b8ff282cda4e0127784cfab8c7f580d70ae343d7
Peter Reutemann <fracpete@gmail.com>
2025-10-31 14:48:25 +1300

    prepared for release

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         "image_dataset_converter",
     ],
-    version="0.0.3",
+    version="0.1.0",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
