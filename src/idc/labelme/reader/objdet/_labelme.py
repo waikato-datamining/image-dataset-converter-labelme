2d84139830768658448ffa3c0ac619b1a34a5fbb
Peter Reutemann <fracpete@gmail.com>
2026-04-21 15:46:23 +1200

    seppl.placeholders has been rebranded as seppl.variables

--- src/idc/labelme/reader/objdet/_labelme.py
+++ src/idc/labelme/reader/objdet/_labelme.py
@@ -7,13 +7,13 @@ import numpy as np
 from wai.logging import LOGGING_WARNING
 from wai.common.adams.imaging.locateobjects import LocatedObjects, LocatedObject
 from wai.common.geometry import Polygon, Point
-from seppl.placeholders import PlaceholderSupporter, placeholder_list
+from seppl.variables import VariableSupporter, variable_list
 from seppl.io import locate_files
 from kasperl.api import Reader
 from idc.api import ObjectDetectionData, locate_image
 
 
-class LabelMeObjectDetectionReader(Reader, PlaceholderSupporter):
+class LabelMeObjectDetectionReader(Reader, VariableSupporter):
 
     def __init__(self, source: Union[str, List[str]] = None, source_list: Union[str, List[str]] = None,
                  resume_from: str = None, logger_name: str = None, logging_level: str = LOGGING_WARNING):
@@ -62,8 +62,8 @@ class LabelMeObjectDetectionReader(Reader, PlaceholderSupporter):
         :rtype: argparse.ArgumentParser
         """
         parser = super()._create_argparser()
-        parser.add_argument("-i", "--input", type=str, help="Path to the json file(s) to read; glob syntax is supported; " + placeholder_list(obj=self), required=False, nargs="*")
-        parser.add_argument("-I", "--input_list", type=str, help="Path to the text file(s) listing the json files to use; " + placeholder_list(obj=self), required=False, nargs="*")
+        parser.add_argument("-i", "--input", type=str, help="Path to the json file(s) to read; glob syntax is supported; " + variable_list(obj=self), required=False, nargs="*")
+        parser.add_argument("-I", "--input_list", type=str, help="Path to the text file(s) listing the json files to use; " + variable_list(obj=self), required=False, nargs="*")
         parser.add_argument("--resume_from", type=str, help="Glob expression matching the file to resume from, e.g., '*/012345.json'", required=False)
         return parser
 
